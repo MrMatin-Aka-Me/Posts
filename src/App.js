a10998b2d043d9d805813ce9330bbfd5c429f6d8
MrMatin-Aka-Me <matindzhuraev@gmail.com>
2021-06-25 15:15:39 +0300

    the second hometask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Message from './components/Message/Message';
+import Post from './components/Post/Post';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Post content="Первый пост" />
+      <Post content="Второй пост" />
+      <Post content="Третий пост" />
+      <Message text="Первое сообщение" />
+      <Message text="Второе сообщение" />
+      <Message text="Третье сообщение" />
     </div>
   );
 }
