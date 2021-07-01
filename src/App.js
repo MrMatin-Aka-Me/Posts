0ce8343044a6998e21c5d97963dcc6b0b725ceef
MrMatin-Aka-Me <matindzhuraev@gmail.com>
2021-07-01 20:40:20 +0300

    H/T #3: Post card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import Message from './components/Message/Message';
+//import Message from './components/Message/Message';
 import Post from './components/Post/Post';
 
 function App() {
+  const post = {
+    id:1,
+    author: {
+      id: 1,
+      avatar: 'https://lms.openjs.io/logo_js.svg',
+      name: 'OpenJS',
+    },
+    content: 'asdfghjkl',
+    photo: 'https://lms.openjs.io/openjs.jpg',
+    hit: true,
+    likes: 100,
+    likedByMe: true,
+    created: 1603501200,
+  }
   return (
     <div className="App">
-      <Post content="Первый пост" />
-      <Post content="Второй пост" />
-      <Post content="Третий пост" />
-      <Message text="Первое сообщение" />
-      <Message text="Второе сообщение" />
-      <Message text="Третье сообщение" />
+      <Post post={post}/>
     </div>
   );
 }
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './Post.css';
 
-function Post(props) {
+function Post({post}) {
+    const {author} = post;
+    const likePhotoUrl = post.likedByMe ? "https://lms.openjs.io/liked.svg" : "https://lms.openjs.io/unliked.svg";     
+
     return (
+    <article>
+        <header>
+            <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name}/>
+            <h5>{author.name}</h5>
+            <div>{post.created}</div>
+            {post.hit && <span>HIT</span>}
+        </header>
         <div>
-            {props.content}
+            <div className="Post-content">{post.content}</div>
+            <img src={post.photo} alt="photo" class="Post-photo"/>
         </div>
+        <footer>
+            <span className="Post-likes">
+                <img src={likePhotoUrl} width="20" height="20"/>
+                <span className="Post-likes-count">{post.likes}</span>
+            </span>
+        </footer>
+    </article>
     )
 }
 
