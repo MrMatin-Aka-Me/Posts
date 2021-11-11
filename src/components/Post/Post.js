3fcf5f5d380d9f121530564d679a22140ea3387f
MrMatin-Aka-Me <matindzhuraev@gmail.com>
2021-11-11 16:10:06 +0300

    created span fo tags

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,14 +2,14 @@ import React from 'react';
 //import PostsContext from '../../contexts/PostsContext';
 import Tags from '../Tags/Tags';
 import './Post.css';
-import {like, remove, hide, edit, show} from '../../store/actions';
-import {useDispatch} from 'react-redux';
+import { like, remove, hide, edit, show } from '../../store/actions';
+import { useDispatch } from 'react-redux';
 
-function Post({post}) {
-    const {author} = post;
-    const {photo} = post;    
+function Post({ post }) {
+    const { author } = post;
+    const { photo } = post;
     const dispatch = useDispatch();
-    
+
     const handleLike = () => {
         dispatch(like(post.id));
         //like(post.id);
@@ -18,8 +18,8 @@ function Post({post}) {
     const handleRemove = () => {
         dispatch(remove(post.id));
         //remove(post.id); 
-    };   
-    
+    };
+
     const handleHide = () => {
         dispatch(hide(post.id));
         //toggleVisibility(post.id);
@@ -39,7 +39,7 @@ function Post({post}) {
         return (
             <article>
                 <header>
-                    <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name}/>
+                    <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name} />
                     <h5>{author.name}</h5>
                     <button onClick={handleShow}>показать</button>
                 </header>
@@ -49,33 +49,35 @@ function Post({post}) {
 
 
     return (
-    <article>
-        <header>
-            <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name}/>
-            <h5>{author.name}</h5>
-            <button onClick={handleRemove}>удалить</button>
-            <button onClick={handleHide}>скрыть</button>
-            <button onClick={handleEdit}>изменить</button>
-            <div>{post.created}</div>
-            {post.hit && <span>HIT</span>}
-        </header>
-        <div>
-            <div className="Post-content">{post.content}</div>
-            {photo && <img src={photo.url} alt={photo.alt} className="Post-photo"/>}
-        </div>
-        <footer>
-            <span className="Post-likes" onClick={handleLike}>
-                <img 
-                src={post.likedByMe ? "https://itchief.ru/assets/images/other/heart.svg" : "https://img.icons8.com/ios/500/like--v2.png"} 
-                alt="likes" 
-                width="20" 
-                height="20"
-                />
-                <span className="Post-likes-count">{post.likes}</span>
-                {post.tags && <Tags tags={post.tags}/>}
-            </span>
-        </footer>
-    </article>
+        <article>
+            <header>
+                <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name} />
+                <h5>{author.name}</h5>
+                <button onClick={handleRemove}>удалить</button>
+                <button onClick={handleHide}>скрыть</button>
+                <button onClick={handleEdit}>изменить</button>
+                <div>{post.created}</div>
+                {post.hit && <span>HIT</span>}
+            </header>
+            <div>
+                <div className="Post-content">{post.content}</div>
+                {photo && <img src={photo.url} alt={photo.alt} className="Post-photo" />}
+            </div>
+            <footer>
+                <span className="Post-likes" onClick={handleLike}>
+                    <img
+                        src={post.likedByMe ? "https://itchief.ru/assets/images/other/heart.svg" : "https://img.icons8.com/ios/500/like--v2.png"}
+                        alt="likes"
+                        width="20"
+                        height="20"
+                    />
+                    <span className="Post-likes-count">{post.likes}</span>
+                </span>
+                <span className="Post-Tags">
+                    {post.tags && <Tags tags={post.tags} />}
+                </span>
+            </footer>
+        </article>
     );
 }
 
